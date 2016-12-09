262685886e3aa51eb728c019153c066e014a8108
Vu Toan <vutoan@Vus-MacBook-Pro.local>
2016-12-09 21:48:33 +0700

    add debouncing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+import _ from 'lodash';
 const API_KEY= 'AIzaSyCE7NNcnNtEGWqD-GarjcjckV-_qISBhEk';
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
     super(props);
 
     this.state = {
+      request: 0,
       videos: [],
       selectedVideo: null
     };
@@ -19,20 +21,25 @@ class App extends Component {
   }
 
   videoSearch (term) {
+    const oldRequest = this.state.request;
     YTSearch({ key: API_KEY, term: term}, (videos) => {
       this.setState({
+        request: oldRequest + 1,
         videos: videos,
         selectedVideo: videos[0]
       });
     });
   }
 
+
   render() {
+    const videoSearch = _.debounce( term => this.videoSearch(term), 2000);
     return (
       <div className="container">
         <br/>
+        <h1>you request {this.state.request} times</h1>
         <div className="col-md-8">
-          <SearchBar onSearchTermChange={ term => this.videoSearch(term) }/>
+          <SearchBar onSearchTermChange={ term => videoSearch(term) }/>
           <VideoDetail video={this.state.selectedVideo}/>
         </div>
         <VideoList
