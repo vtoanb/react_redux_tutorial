129a1cebf7697dca24f780210fc66188ac0bebbb
Vu Toan <vutoan@Vus-MacBook-Pro.local>
2016-12-09 19:07:22 +0700

    video detail part

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY= 'AIzaSyCE7NNcnNtEGWqD-GarjcjckV-_qISBhEk';
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.video}/>
         <VideoList videos={this.state.videos} />
       </div>
     );
