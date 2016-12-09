681221fb663fdbe30a1ccdca9a69233cdab0eb41
Vu Toan <vutoan@Vus-MacBook-Pro.local>
2016-12-09 17:14:18 +0700

    refactor code by extends React.Components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar'; 
+const API_KEY= 'AIzaSyCE7NNcnNtEGWqD-GarjcjckV-_qISBhEk';
 
-const API_KEY= 'AIzaSyCE7NNcnNtEGWqD-GarjcjckV-_qISBhEk'
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-YTSearch({ key: API_KEY, term: 'surfboards' }, function(data){
-  console.log(data);
-});
+    this.state = { videos: [] };
 
-const App = () => {
-  return(
-    <div>
-      <SearchBar />
-    </div>);
+    YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos: videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector(".container"));
