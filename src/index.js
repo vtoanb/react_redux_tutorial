6d2da1f2c6c54b1297d35ea472a05eab0638b347
Vu Toan <vutoan@Vus-MacBook-Pro.local>
2016-12-09 14:57:59 +0700

    basic project structure and webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar'; 
+
+const API_KEY= 'AIzaSyCE7NNcnNtEGWqD-GarjcjckV-_qISBhEk'
+
 // Create a new import React from 'react'
 // some Html
 
+const App = () => {
+  return(
+    <div>
+      <SearchBar />
+    </div>);
+}
 // Take this component's generator
 // on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector(".container"));
