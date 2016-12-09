cb6ac138d2ebf63db7f2cf5402524c701b83b76d
Vu Toan <vutoan@Vus-MacBook-Pro.local>
2016-12-09 17:02:33 +0700

    part 22, using youtube api

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,8 +3,23 @@ import React, { Component } from 'react';
 
 // class SearchBar extends React.Component { replace by ES6 syntax
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
   render() {
-    return <input />
+    return (
+      <div>
+        <input
+          value = {this.state.term}
+          onChange={event => this.setState({ term: event.target.value })} />
+      </div>
+    );
+  }
+  // event handler, method with es6
+  onInputChange(event) {
+    console.log(event.target.value);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar'; 
 
 const API_KEY= 'AIzaSyCE7NNcnNtEGWqD-GarjcjckV-_qISBhEk'
 
-// Create a new import React from 'react'
-// some Html
+YTSearch({ key: API_KEY, term: 'surfboards' }, function(data){
+  console.log(data);
+});
 
 const App = () => {
   return(
@@ -14,6 +16,5 @@ const App = () => {
       <SearchBar />
     </div>);
 }
-// Take this component's generator
-// on the page (in the DOM)
+
 ReactDOM.render(<App />, document.querySelector(".container"));
