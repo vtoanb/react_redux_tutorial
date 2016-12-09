b6a6b4a12ea4c5f568024b7246af63cb91f0ddf2
Vu Toan <vutoan@Vus-MacBook-Pro.local>
2016-12-09 19:52:50 +0700

    on selected video

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
   const imageUrl = video.snippet.thumbnails.default.url;
   const videoTitle = video.snippet.title;
 
   return(
-      <li className="list-group-item">
+      <li onClick={() => onVideoSelect(video)} className="list-group-item">
         <div className="video-list media">
           <div className="media-left">
             <img className="media-object" src={imageUrl}/>
--- src/index.js
+++ src/index.js
@@ -10,18 +10,26 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
   render() {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
       </div>
     );
   }
