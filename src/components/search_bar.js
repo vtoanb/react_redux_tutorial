ac54cc12882ca859b1667c015e258dec18da4ae6
Vu Toan <vutoan@Vus-MacBook-Pro.local>
2016-12-09 15:00:48 +0700

    class component and ES6 syntax

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,12 @@
-import React from 'react';
+// import React from 'react'; replace by ES6 syntax
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return <input />
-};
+// class SearchBar extends React.Component { replace by ES6 syntax
+class SearchBar extends Component {
+  render() {
+    return <input />
+  }
+}
+
 
 export default SearchBar;
